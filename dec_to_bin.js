98ee1c6adb0f437b72e790139bbec30b541f5f37
ognjen987 <ognjen987@gmail.com>
2016-11-06 16:55:13 +0100

    Decimal to binary

---
+++ dec_to_bin.js
@@ -0,0 +1,8 @@
+//Decimal to binary 
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line !== "") {
+        console.log(parseInt(line, 10).toString(2));
+    }
+
+});
